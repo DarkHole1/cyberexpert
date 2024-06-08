151475ddf6af365881cebd8f983ef23b85b87a62
DarkHole1 <dark.hole1@yandex.ru>
2024-06-08 14:14:12 +0300

    Add request params

--- src/main/java/com/example/demo/PurchaseController.java
+++ src/main/java/com/example/demo/PurchaseController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +13,9 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping("/create-order")
-    public Status createOrder() {
+    public Status createOrder(@RequestParam Map<String, String> allParams) {
         try {
-            purchaseService.createOrder();
+            purchaseService.createOrder(allParams);
             return new Status(true, "Payment created");
         } catch(UnsupportedOperationException e) {
             return new Status(false, "Not implemented");
--- src/main/java/com/example/demo/PurchaseService.java
+++ src/main/java/com/example/demo/PurchaseService.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PurchaseService {
-    public void createOrder() {
+    public void createOrder(Map<String, String> params) {
         throw new UnsupportedOperationException();
     }
 }
