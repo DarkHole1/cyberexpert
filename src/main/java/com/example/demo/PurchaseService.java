163b8f3ce5af5fe01afe0f674bf5733cf5e24203
DarkHole1 <dark.hole1@yandex.ru>
2024-06-08 14:39:19 +0300

    Add calculate signature stub

--- src/main/java/com/example/demo/PurchaseService.java
+++ src/main/java/com/example/demo/PurchaseService.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Service;
 public class PurchaseService {
     public void createOrder(Map<String, String> params) {
         // Generate signature
+        String signature = calculateSignature(params, "00112233445566778899aabbccddeeff")
         // Send to modulebank
         // Save to database
+    }
+    
+    private String calculateSignature(Map<String, String> params, String key) {
         throw new UnsupportedOperationException();
     }
 }
