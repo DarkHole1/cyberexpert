7ac268ef196522f4388ceebe5966dc017fd2c3c7
DarkHole1 <dark.hole1@yandex.ru>
2024-06-08 14:23:50 +0300

    Organise code

--- src/main/java/com/example/demo/PurchaseService.java
+++ src/main/java/com/example/demo/PurchaseService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class PurchaseService {
     public void createOrder(Map<String, String> params) {
+        // Generate signature
+        // Send to modulebank
+        // Save to database
         throw new UnsupportedOperationException();
     }
 }
