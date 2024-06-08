3f73f73fa16ba4846cfde03078c91a88f4059ea1
DarkHole1 <dark.hole1@yandex.ru>
2024-06-08 15:40:31 +0300

    Add signature calculation

--- src/main/java/com/example/demo/PurchaseService.java
+++ src/main/java/com/example/demo/PurchaseService.java
@@ -1,19 +1,45 @@
 package com.example.demo;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Comparator;
+import java.util.HexFormat;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class PurchaseService {
-    public void createOrder(Map<String, String> params) {
+    public void createOrder(Map<String, String> params) throws NoSuchAlgorithmException {
         // Generate signature
         String signature = calculateSignature(params, "00112233445566778899aabbccddeeff");
         // Send to modulebank
         // Save to database
     }
-    
-    private String calculateSignature(Map<String, String> params, String key) {
-        throw new UnsupportedOperationException();
+
+    private String calculateSignature(Map<String, String> params, String key) throws NoSuchAlgorithmException {
+        String queryString = params.entrySet().stream()
+                .filter(entry -> entry.getValue().length() > 0)
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .map(entry -> String.format("%s=%s",
+                        entry.getKey(),
+                        Base64.getEncoder().encodeToString(entry.getValue().getBytes())))
+                .collect(Collectors.joining("&"));
+
+        return calculateDigest(queryString, key);
+    }
+
+    private String calculateDigest(String queryString, String key) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(key.getBytes());
+        md.update(queryString.getBytes());
+        byte[] digest = md.digest();
+        md.reset();
+        md.update(key.getBytes());
+        md.update(digest);
+        byte[] result = md.digest();
+        return HexFormat.of().formatHex(result);
     }
 }
